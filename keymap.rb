9ce80c750dea97905f6b18668a020dced6a5045f
Akihiro Kurotani <kurod1492@gmail.com>
2022-09-14 00:39:14 +0900

    modify keymap.rb

--- keymap.rb
+++ keymap.rb
@@ -19,11 +19,11 @@ kbd.init_matrix_pins(
 
 #default layer should be added at first
 kbd.add_layer :default, %i(
-KC_ESC  KC_1 KC_2 KC_3    KC_4    KC_5                    KC_6  KC_7  KC_8     KC_9    KC_0     KC_BSPC
-KC_TAB  KC_Q KC_W KC_E    KC_R    KC_T                    KC_Y  KC_U  KC_I     KC_O    KC_P     KC_MINUS
-KC_LCTL KC_A KC_S KC_D    KC_F    KC_G                    KC_H  KC_J  KC_K     KC_L    KC_SCLN  KC_QUOT
-KC_LSFT KC_Z KC_X KC_C    KC_V    KC_B   KC_LBRC KC_B     KC_N  KC_M  KC_COMMA KC_DOT  KC_SLASH KC_RSFT
-                  KC_LGUI KC_LALT LOWER  KC_SPC  KC_ENTER RAISE KC_UP KC_RIGHT
+KC_ESC  KC_1 KC_2 KC_3    KC_4    KC_5                 KC_6     KC_7  KC_8     KC_9    KC_0     KC_BSPC
+KC_TAB  KC_Q KC_W KC_E    KC_R    KC_T                 KC_Y     KC_U  KC_I     KC_O    KC_P     KC_MINUS
+KC_LCTL KC_A KC_S KC_D    KC_F    KC_G                 KC_H     KC_J  KC_K     KC_L    KC_SCLN  KC_QUOT
+KC_LSFT KC_Z KC_X KC_C    KC_V    KC_B   KC_LBRC KC_B  KC_N     KC_M  KC_COMMA KC_DOT  KC_SLASH KC_RSFT
+                  KC_LGUI KC_LALT KC_SPC LOWER   RAISE KC_ENTER KC_UP KC_RIGHT
 KC_9   KC_8   KC_7
 KC_6   KC_5   KC_4 KC_PLUS
 KC_3   KC_2   KC_1 KC_MINUS
@@ -35,7 +35,7 @@ KC_TILD KC_F1 KC_AT KC_F3   KC_F4   KC_F5                   KC_F6   KC_F7   KC_F
 KC_TAB  KC_Q  KC_W  KC_E    KC_R    KC_T                    KC_Y    KC_U    KC_I     KC_O     KC_P      KC_EQLS
 KC_LCTL KC_A  KC_S  KC_D    KC_F    KC_G                    KC_LEFT KC_DOWN KC_UP    KC_RIGHT KC_SCOLON KC_PLUS
 KC_LSFT KC_Z  KC_X  KC_C    KC_V    KC_B   KC_RBRC KC_B     KC_N    KC_M    KC_COMMA KC_DOT   KC_SLASH  KC_RSFT
-                    KC_LGUI KC_LALT LOWER  KC_SPC  KC_ENTER RAISE   KC_UP KC_RIGHT
+                    KC_LGUI KC_LALT KC_SPC LOWER   RAISE KC_ENTER KC_UP KC_RIGHT
 KC_9   KC_8   KC_7
 KC_6   KC_5   KC_4 KC_PLUS
 KC_3   KC_2   KC_1 KC_MINUS
@@ -47,7 +47,7 @@ KC_GRAVE KC_F1 KC_F2 KC_F3   KC_F4   KC_F5                   KC_F6    KC_F7   KC
 KC_TAB   KC_Q  KC_W  KC_E    KC_R    KC_T                    KC_Y     KC_U    KC_UP    KC_F11   KC_F12    KC_EQLS
 KC_LCTL  KC_A  KC_S  KC_D    KC_F    KC_G                    KC_LEFT  KC_LEFT KC_DOWN  KC_RIGHT KC_SCOLON KC_EQLS
 KC_LSFT  KC_Z  KC_X  KC_C    KC_V    KC_B   KC_RCBR KC_RBRC  KC_N     KC_M    KC_COMMA KC_DOT   KC_SLASH  KC_RSFT
-                     KC_LGUI KC_LALT LOWER  KC_SPC  KC_ENTER RAISE KC_UP KC_RIGHT
+                     KC_LGUI KC_LALT KC_SPC LOWER   RAISE KC_ENTER KC_UP KC_RIGHT
 KC_9   KC_8   KC_7
 KC_6   KC_5   KC_4 KC_PLUS
 KC_3   KC_2   KC_1 KC_MINUS
