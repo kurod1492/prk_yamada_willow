74ca916c739e0d4c6d0f1c763f14d9860763463f
kuro <kuro@netlab.jp>
2022-11-12 23:52:55 +0900

    add LED and rotary encoder

--- keymap.rb
+++ keymap.rb
@@ -21,46 +21,50 @@ kbd.init_matrix_pins(
 
 #default layer should be added at first
 kbd.add_layer :default, %i(
-KC_ESC   KC_1     KC_2      KC_3     KC_4     KC_5                       KC_6       KC_7     KC_8     KC_9     KC_0      KC_BSPC
-KC_TAB   KC_Q     KC_W      KC_E     KC_R     KC_T                       KC_Y       KC_U     KC_I     KC_O     KC_P      KC_QUOT
-KC_LCTL  KC_A     KC_S      KC_D     KC_F     KC_G                       KC_H       KC_J     KC_K     KC_L     KC_SCOLON KC_SLASH
-KC_LSFT  KC_Z     KC_X      KC_C     KC_V     KC_B     KC_LBRC  KC_B     KC_N       KC_M     KC_COMMA KC_DOT   KC_RIGHT  KC_RSFT
-                            KC_LGUI  KC_LALT  LOWER    KC_SPC   KC_ENTER LEFT_RAISE KC_DOWN  KC_UP
+KC_ESC   KC_1     KC_2      KC_3     KC_4      KC_5                          KC_6       KC_7     KC_8     KC_9     KC_0      KC_BSPC
+KC_TAB   KC_Q     KC_W      KC_E     KC_R      KC_T                          KC_Y       KC_U     KC_I     KC_O     KC_P      KC_QUOT
+KC_LCTL  KC_A     KC_S      KC_D     KC_F      KC_G                          KC_H       KC_J     KC_K     KC_L     KC_SCOLON KC_RSFT
+KC_LSFT  KC_Z     KC_X      KC_C     KC_V      KC_B    KC_LBRC    KC_RBRC    KC_N       KC_M     KC_COMMA KC_DOT   KC_SLASH  KC_RIGHT
+                            KC_LALT  KC_LGUI   LOWER   KC_SPC     KC_ENTER   RAISE_LEFT KC_DOWN  KC_UP
 KC_9  KC_8     KC_7
 KC_6  KC_5     KC_4   KC_PLUS
 KC_3  KC_2     KC_1   KC_MINUS
       KC_DOT   KC_0
+KC_NO RGB_RMOD RGB_MOD KC_F13 KC_F14 KC_F15 KC_F16 KC_F17 KC_F18
 )
 
 kbd.add_layer :lower, %i(
-KC_GRAVE KC_F1    KC_AT     KC_F3    KC_F4    KC_F5                      KC_F6     KC_F7      KC_F8    KC_F9    KC_F10    KC_BSPC
-KC_TAB   KC_Q     KC_W      KC_E     KC_R     KC_T                       KC_Y      KC_U       KC_I     KC_F11   KC_F12    KC_BSLASH
-KC_LCTL  KC_A     KC_S      KC_D     KC_F     KC_G                       KC_H      KC_UNDS    KC_EQUAL KC_RIGHT KC_COLON  KC_NO
-KC_LSFT  KC_Z     KC_X      KC_C     KC_V     KC_B     KC_LBRC  KC_RBRC  KC_N      KC_M       KC_COMMA KC_BRIU  KC_VOLU  KC_RSFT
-                            KC_LGUI  KC_LALT  KC_NO    KC_NO    KC_NO    KC_SCOLON KC_VOLD    KC_BRID 
+KC_GRAVE KC_F1    KC_AT     KC_F3    KC_F4     KC_F5                         KC_F6     KC_F7     KC_F8    KC_F9    KC_F10    KC_BSPC
+KC_TAB   KC_Q     KC_W      KC_E     KC_R      KC_T                          KC_Y      KC_U      KC_I     KC_F11   KC_P      KC_BSLASH
+KC_LCTL  KC_A     KC_S      KC_D     KC_F      KC_G                          KC_H   KC_MINUS  KC_PLUS  KC_EQUAL KC_RABK   KC_RSFT
+KC_LSFT  KC_Z     KC_X      KC_C     KC_V      KC_B     KC_LABK   KC_B       KC_N      KC_M      KC_RABK  KC_NO    KC_NO     KC_VOLU
+                            KC_LALT  KC_LGUI   KC_NO    KC_NO     KC_NO      KC_VOLD   KC_BRID   KC_BRIU
 KC_PGUP   KC_UP     KC_HOME
 KC_RIGHT  KC_5      KC_LEFT  KC_PLUS
 KC_PGDOWN KC_DOWN   KC_END   KC_MINUS
           KC_DELETE KC_INSERT
+KC_NO RGB_RMOD RGB_MOD KC_F13 KC_F14 KC_F15 KC_F16 KC_F17 KC_F18
 )
 
 kbd.add_layer :raise, %i(
-KC_TILD  KC_F1    KC_F2     KC_F3    KC_F4    KC_F5                      KC_F6    KC_F7    KC_F8    KC_F9    KC_F10    KC_BSPC
-KC_TAB   BOOTSEL  KC_W      KC_E     KC_R     KC_T                       KC_Y     KC_U     KC_I     KC_HOME  KC_END    KC_PIPE
-KC_LCTL  KC_A     KC_S      KC_D     KC_F     KC_G                       KC_LEFT  KC_MINUS KC_PLUS  KC_RIGHT KC_SCOLON KC_NO
-KC_LSFT  KC_Z     KC_X      KC_C     KC_V     KC_B     KC_LCBR  KC_RCBR  KC_N     KC_M     KC_COMMA KC_DOT   KC_SLASH  KC_RSFT
-                            KC_LGUI  KC_LALT  KC_NO    KC_NO    KC_NO    KC_NO    KC_NO    KC_NO 
+KC_TILD  KC_F1    KC_F2     KC_F3    KC_F4    KC_F5                          KC_F6     KC_F7     KC_F8    KC_F9    KC_F10    KC_BSPC
+KC_TAB   BOOTSEL  KC_W      KC_E     KC_R     KC_T                           KC_PGDOWN KC_PGUP   KC_HOME  KC_END   KC_NO     KC_PIPE
+KC_LCTL  KC_A     KC_S      KC_D     KC_F     KC_G                           KC_H  KC_UNDS   KC_EQUAL KC_PLUS  KC_NO     KC_RSFT
+KC_LSFT  KC_Z     KC_X      KC_C     KC_V     KC_B     KC_RABK    KC_B       KC_N      KC_M      KC_COMMA KC_DOT   KC_SLASH  KC_VOLU
+                            KC_LALT  KC_LGUI  KC_NO    KC_NO      KC_NO      KC_NO     KC_PGDOWN KC_PGUP
 KC_9   KC_8   KC_7
 KC_6   KC_5   KC_4 KC_PLUS
 KC_3   KC_2   KC_1 KC_MINUS
        KC_DOT KC_0
+KC_NO RGB_RMOD RGB_MOD KC_F13 KC_F14 KC_F15 KC_F16 KC_F17 KC_F18
 )
-kbd.define_mode_key :LOWER, [ nil, :lower, nil, nil ]
-#kbd.define_mode_key :RAISE, [ nil, :raise, nil, nil ]
-kbd.define_mode_key :BOOTSEL, [ Proc.new { kbd.bootsel! }, nil, 200, nil ]
 
-kbd.define_mode_key :RAISE_SCOLON,[ %i(KC_SCOLON), :raise, 300, 150 ]
-kbd.define_mode_key :LEFT_RAISE,[ %i(KC_LEFT), :raise, 300, 150 ]
+kbd.define_mode_key :LOWER,        [ nil,                       :lower,   nil, nil ]
+kbd.define_mode_key :RAISE,        [ nil,                       :raise,   nil, nil ]
+kbd.define_mode_key :SPC_LOWER,    [ :KC_SPC,                   :lower,   150, 150 ]
+kbd.define_mode_key :ENT_RAISE,    [ :KC_ENTER,                 :raise,   150, 150 ]
+kbd.define_mode_key :RAISE_LEFT,   [ :KC_LEFT,                  :raise,   150, 150 ]
+kbd.define_mode_key :BOOTSEL,      [ Proc.new { kbd.bootsel! }, nil,      200, nil ]
 #                                 ^^^^^^^^^^^^^^^^^^^^  ^^^^^^^  ^^^  ^^^
 #                                     (1)                   (2)      (3)  (4)
 # (1): Symbol of a keycode, Array of multiple keycodes, or Proc which is going
@@ -71,5 +75,75 @@ kbd.define_mode_key :LEFT_RAISE,[ %i(KC_LEFT), :raise, 300, 150 ]
 #      (1) key is going to be invoked once.
 # (4): Re-push time threshold(ms). Under the state of (3), if you re-push the
 #      key within the time, (1) key is going to be kept pressed
+
+#     PINS = {
+#  'D1' => 2,
+#  'D0' => 3,
+#  'D4' => 4,
+#  'C6' => 5,
+#  'D7' => 6,
+#  'E6' => 7,
+#  'B4' => 8,
+#  'B5' => 9,
+#  'F4' => 29,
+#  'F5' => 28,
+#  'F6' => 27,
+#  'F7' => 26,
+#  'B1' => 22,
+#  'B3' => 20,
+#  'B2' => 23,
+#  'B6' => 21,
+#}
+# D4-C6 => 4, 5
+# D7-E6 => 6, 7
+# B4-B5 => 8, 9
+
+
+encoder_1 = RotaryEncoder.new(4, 5)
+encoder_1.counterclockwise do
+  kbd.send_key :RGB_RMOD
+end
+encoder_1.clockwise do
+  kbd.send_key :RGB_MOD
+end
+kbd.append encoder_1
+
+encoder_2 = RotaryEncoder.new(6, 7)
+encoder_2.counterclockwise do
+  kbd.send_key :KC_PGUP
+end
+encoder_2.clockwise do
+  kbd.send_key :KC_PGDOWN
+end
+kbd.append encoder_2
+
+encoder_3 = RotaryEncoder.new(8, 9)
+encoder_3.counterclockwise do
+  kbd.send_key :KC_UP
+end
+encoder_3.clockwise do
+  kbd.send_key :KC_DOWN
+end
+kbd.append encoder_3
+
+
+rgb = RGB.new(0, 58, 41)
+[
+  [0, 0],  [20, 0],  [40, 0],  [60, 0],  [80, 0],  [100, 0],  [120, 0],  [140, 0],  [160, 0],  [180, 0],  [200, 0],  [220, 0], 
+  [0, 16], [20, 16], [40, 16], [60, 16], [80, 16], [100, 16], [120, 16], [140, 16], [160, 16], [180, 16], [200, 16], [220, 16],
+  [0, 32], [20, 32], [40, 32], [60, 32], [80, 32], [100, 32], [120, 32], [140, 32], [160, 32], [180, 32], [200, 32], [220, 32],
+  [0, 48], [17, 48], [34, 48], [51, 48], [68, 48], [85,  48], [102, 48], [119, 48], [138, 48], [155, 48], [172, 48], [189, 48], [206, 48], [224, 48],
+  [51, 64], [68, 64], [85, 64], [102, 64], [119, 64], [138, 64], [155, 64],[172, 64]
+].each do |p|
+  rgb.add_pixel(p[0], p[1])
+end
+
+rgb.effect     = :circle
+rgb.speed      = 22  # 1-31  / default: 22
+rgb.hue        = 10  # 0-100 / default: 0
+rgb.saturation = 100 # 0-100 / default: 100
+rgb.value      = 10  # 1-31  / default: 13
+
+kbd.append rgb # `kbd` is an instance of Keyboard class that should be newed in advance
 
 kbd.start!
