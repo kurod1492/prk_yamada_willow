cc27eacb88746889970cf892a773420eb104f57a
Akihiro Kurotani <kurod1492@gmail.com>
2022-12-12 11:59:15 +0900

    modify keymap

--- keymap.rb
+++ keymap.rb
@@ -22,10 +22,23 @@ kbd.init_matrix_pins(
 #default layer should be added at first
 kbd.add_layer :default, %i(
 KC_ESC   KC_1     KC_2      KC_3     KC_4      KC_5                          KC_6       KC_7     KC_8     KC_9     KC_0      KC_BSPC
-KC_TAB   KC_Q     KC_W      KC_E     KC_R      KC_T                          KC_Y       KC_U     KC_I     KC_O     KC_P      KC_QUOT
-KC_LCTL  KC_A     KC_S      KC_D     KC_F      KC_G                          KC_H       KC_J     KC_K     KC_L     KC_SCOLON KC_RSFT
+KC_TAB   KC_Q     KC_W      KC_E     KC_R      KC_T                          KC_Y       KC_U     KC_I     KC_O     KC_P      KC_BSLASH
+KC_LCTL  KC_A     KC_S      KC_D     KC_F      KC_G                          KC_H       KC_J     KC_K     KC_L     KC_SCOLON KC_QUOT
 KC_LSFT  KC_Z     KC_X      KC_C     KC_V      KC_B    KC_LBRC    KC_RBRC    KC_N       KC_M     KC_COMMA KC_DOT   KC_SLASH  KC_RIGHT
-                            KC_LALT  KC_LGUI   LOWER   KC_SPC     KC_ENTER   RAISE_LEFT KC_DOWN  KC_UP
+                            KC_LALT  KC_LGUI   KC_LBRC SPC_LOWER  ENT_RAISE  KC_LEFT KC_DOWN  KC_UP
+KC_PGUP   KC_UP     KC_HOME
+KC_RIGHT  KC_K      KC_LEFT  KC_PLUS
+KC_PGDOWN KC_DOWN   KC_END   KC_MINUS
+          KC_DELETE KC_INSERT
+KC_NO KC_F3 KC_F4 KC_F5
+)
+
+kbd.add_layer :lower, %i(
+KC_GRAVE KC_F1    KC_AT     KC_F3    KC_F4     KC_F5                         KC_F6     KC_F7     KC_F8    KC_F9    KC_F10    KC_PLUS
+KC_TAB   KC_Q     KC_W      KC_E     KC_R      KC_T                          KC_Y      KC_U      KC_I     KC_F11   KC_P      KC_BSLASH
+KC_LCTL  KC_A     KC_S      KC_D     KC_F      KC_G                          KC_H      KC_MINUS  KC_PLUS  KC_EQUAL KC_RABK   KC_QUOT
+KC_LSFT  KC_Z     KC_X      KC_C     KC_V      KC_B     KC_LABK   KC_B       KC_N      KC_M      KC_RABK  KC_NO    KC_NO     KC_VOLU
+                            KC_LALT  KC_LGUI   KC_NO    KC_NO     KC_NO      KC_VOLD   KC_BRID   KC_BRIU
 KC_9  KC_8     KC_7
 KC_6  KC_5     KC_4   KC_PLUS
 KC_3  KC_2     KC_1   KC_MINUS
@@ -33,23 +46,10 @@ KC_3  KC_2     KC_1   KC_MINUS
 KC_NO KC_F3 KC_F4 KC_F5
 )
 
-kbd.add_layer :lower, %i(
-KC_GRAVE KC_F1    KC_AT     KC_F3    KC_F4     KC_F5                         KC_F6     KC_F7     KC_F8    KC_F9    KC_F10    KC_BSPC
-KC_TAB   KC_Q     KC_W      KC_E     KC_R      KC_T                          KC_Y      KC_U      KC_I     KC_F11   KC_P      KC_BSLASH
-KC_LCTL  KC_A     KC_S      KC_D     KC_F      KC_G                          KC_H   KC_MINUS  KC_PLUS  KC_EQUAL KC_RABK   KC_RSFT
-KC_LSFT  KC_Z     KC_X      KC_C     KC_V      KC_B     KC_LABK   KC_B       KC_N      KC_M      KC_RABK  KC_NO    KC_NO     KC_VOLU
-                            KC_LALT  KC_LGUI   KC_NO    KC_NO     KC_NO      KC_VOLD   KC_BRID   KC_BRIU
-KC_PGUP   KC_UP     KC_HOME
-KC_RIGHT  KC_5      KC_LEFT  KC_PLUS
-KC_PGDOWN KC_DOWN   KC_END   KC_MINUS
-          KC_DELETE KC_INSERT
-KC_NO KC_F3 KC_F4 KC_F5
-)
-
 kbd.add_layer :raise, %i(
-KC_TILD  KC_F1    KC_F2     KC_F3    KC_F4    KC_F5                          KC_F6     KC_F7     KC_F8    KC_F9    KC_F10    KC_BSPC
+KC_TILD  KC_F1    KC_F2     KC_F3    KC_F4    KC_F5                          KC_F6     KC_F7     KC_F8    KC_F9    KC_F10    KC_EQUAL
 KC_TAB   BOOTSEL  KC_W      KC_E     KC_R     KC_T                           KC_PGDOWN KC_PGUP   KC_HOME  KC_END   KC_NO     KC_PIPE
-KC_LCTL  KC_A     KC_S      KC_D     KC_F     KC_G                           KC_H  KC_UNDS   KC_EQUAL KC_PLUS  KC_NO     KC_RSFT
+KC_LCTL  KC_A     KC_S      KC_D     KC_F     KC_G                           KC_H      KC_UNDS   KC_EQUAL KC_PLUS  KC_NO     KC_QUOT
 KC_LSFT  KC_Z     KC_X      KC_C     KC_V     KC_B     KC_RABK    KC_B       KC_N      KC_M      KC_COMMA KC_DOT   KC_SLASH  KC_VOLU
                             KC_LALT  KC_LGUI  KC_NO    KC_NO      KC_NO      KC_NO     KC_PGDOWN KC_PGUP
 KC_9   KC_8   KC_7
